570d79afbf436edd1ebbbd9535ca197aa964305a
Jevone12 <156984927+Jevone12@users.noreply.github.com>
2025-03-06 12:56:22 -0500

    MAR 6

--- UNITY QUIZ/Assets/Complex State Machine/EnemyController.cs
+++ UNITY QUIZ/Assets/Complex State Machine/EnemyController.cs
@@ -2,148 +2,99 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-
-    private enum State
+    private enum State 
     {
         Pace,
         Follow,
     }
 
-    [SerializeField]
-    GameObject[] route;
-    GameObject target;
-
-    [SerializeField]
-    
-    int routeIndex = 0;
-
-    [SerializeField]
-    int health = 10;
-
-    [SerializeField]
-    float speed = 1.0f;
     private State currentState = State.Pace;
 
-    Animator anim;
+    [SerializeField]
+    GameObject[] route;
+
+    int routeIndex = 0;
+    GameObject target;
+    public float speed = 1.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        anim = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        switch(currentState)
+        switch(currentState) 
         {
             case State.Pace:
-                OnPace();
+                Pacing();
                 break;
             case State.Follow:
-                OnFollow();
+                Following();
                 break;
-        }
-
-        if (health <= 0)
-        {
-            Destroy(gameObject);
         }
     }
 
-    void OnPace()
+    void Pacing() 
     {
-        anim.SetBool("Following", false);
         //What do we do when we're pacing?
-        print("I'm pacing!");
         target = route[routeIndex];
-
         MoveTo(target);
 
-        if (Vector3.Distance(transform.position, target.transform.position) < 0.1)
+        if (Vector3.Distance(transform.position, target.transform.position) < 0.001f) 
         {
             routeIndex += 1;
-
             if (routeIndex >= route.Length)
             {
                 routeIndex = 0;
             }
         }
 
-
-     //On what condition do we switch states?
-
-
+        //On what condition do we switch states?
         GameObject obstacle = CheckForward();
-
-        if (obstacle != null)
+        if (obstacle != null) 
         {
             target = obstacle;
-            print("switching");
             currentState = State.Follow;
         }
-   
     }
 
-    void OnFollow()
+    void Following() 
     {
-        anim.SetBool("Following", true);
-        //What do we do when we're following?
-        print("I'm following!");
         MoveTo(target);
 
         //On what condition do we switch states?
         GameObject obstacle = CheckForward();
-        
-        if (obstacle == null)
+        if (obstacle == null) 
         {
             currentState = State.Pace;
         }
-
     }
 
-      void OnTriggerEnter(Collider other)
+    void MoveTo(GameObject t) 
     {
-        if (other.GetComponent<bullet>() != null) 
-        {
-            health -= 1;
-            Destroy(other.gameObject);
-        }
+        transform.LookAt(t.transform, Vector3.up);
+        transform.position = Vector3.MoveTowards(transform.position, t.transform.position, speed * Time.deltaTime);
     }
 
-
-
-
-
-
-
-    
-    void MoveTo(GameObject t)
-    {
-     transform.position = Vector3.MoveTowards(transform.position, t.transform.position, speed * Time.deltaTime);
-     transform.LookAt(t.transform, Vector3.up);
-    }
-
-    GameObject CheckForward()
+    GameObject CheckForward() 
     {
         RaycastHit hit;
-        Debug.DrawRay(transform.position, transform.forward * 10, Color.magenta);
-
-        if(Physics.Raycast(transform.position, transform.forward, out hit, 10))
+        Debug.DrawRay(transform.position, transform.forward * 10, Color.green);
+        if(Physics.Raycast(transform.position, transform.forward, out hit, 10)) 
         {
             PlayerStateManager player = hit.transform.gameObject.GetComponent<PlayerStateManager>();
-
-            if (player!= null)
+            if (player != null) 
             {
-                if (player.currentState != player.sneakState)
-                {
-                    print(hit.transform.name);
+                //if (player.currentState != player.sneakState) 
+                //{
+                    print(hit.transform.gameObject.name);
                     return hit.transform.gameObject;
-                }
+                //}
             }
         }
-
         return null;
     }
 }
-
-
